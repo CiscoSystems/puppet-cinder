6f537b667c88d40fb40d3fa559616cbe589e8411
dotalton <dotalton@cisco.com>
2013-08-02 08:26:05 -0700

    fixing init override paths

--- manifests/volume/rbd.pp
+++ manifests/volume/rbd.pp
@@ -42,11 +42,11 @@ class cinder::volume::rbd (
 
   case $::osfamily {
     'Debian': {
-      file { $ceph_init_override: ensure => present }
+      $override_line    = "env CEPH_ARGS=\"--id ${rbd_user}\""
       file { '/etc/init/cinder-volume.override': ensure => present }
     }
     'RedHat': {
-      file { $ceph_init_override: ensure => present }
+      $override_line    = "export CEPH_ARGS=\"--id ${rbd_user}\""
       file { '/etc/sysconfig/openstack-cinder-volume': ensure => present }
     }
     default: {
